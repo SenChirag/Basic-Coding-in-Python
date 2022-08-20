520be78658e85cf941497cf16c88d58939db2cf4
Chirag Sen <chiragsen0104@gmail.com>
2022-08-20 23:15:38 +0530

    fifth code

---
+++ All_Operations.py
@@ -0,0 +1,18 @@
+a = int(input("Insert a number: "))
+b = int(input("Insert another number: "))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+flr_div = a // b
+mod = a % b
+power = pow(a,b)
+
+print("sum: ", sum)
+print("Substraction: ",sub)
+print("multipliction: ",mul)
+print("division: ",div)
+print("floor division: ",flr_div)
+print("remainder: ",mod)
+print("power: ",power)
