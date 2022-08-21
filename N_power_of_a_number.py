21981570362a31f3ec9ddd89cd02d55fcf1f931b
Chirag Sen <chiragsen0104@gmail.com>
2022-08-21 14:54:09 +0530

    twelfth code

---
+++ N_power_of_a_number.py
@@ -0,0 +1,5 @@
+base = float(input("Enter the value of base: "))
+expo = float(input("Enter the value of exponent: "))
+temp = pow(base,expo)
+
+print(str(base)," to the power ", str(expo), " is", temp)
